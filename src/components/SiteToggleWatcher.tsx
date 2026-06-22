c86984be69d1bad17e29f39ae002c3968a4cc2ed
alienoop05 <alienoop05@gmail.com>
2026-06-22 09:25:41 +0530

    added toggler

--- src/components/SiteToggleWatcher.tsx
+++ src/components/SiteToggleWatcher.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-const BACKEND_URL = 'http://localhost:4000';
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export default function SiteToggleWatcher() {
   useEffect(() => {
